63adf054983aad6648fa95c519db4aafc0aa4b3b
chaitanya@Chaitanyas-MacBook-Pro.local <17334965+chai-kat@users.noreply.github.com>
2022-03-02 04:20:26 +0100

    fixed maze generation header file

--- maze_functions.h
+++ maze_functions.h
@@ -1,6 +1,12 @@
 #ifndef MAZE_FUNCTIONS_H
 #define MAZE_FUNCTIONS_H
 
-uint32_t* generate_maze();
+void generate_blank_cell_array(uint32_t* cell_array);
+void remove_vertical(uint32_t* cell_array, uint8_t x, uint8_t y);
+void remove_horizontal(uint32_t* cell_array, uint8_t x, uint8_t y);
+
+bool rand_tf();
+
+void generate_maze(uint32_t* screen);
 
 #endif
