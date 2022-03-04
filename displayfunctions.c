e1ede1d656bfa6dfb801c45ea6db44707128f3da
adammeh <adammehdi735@gmail.com>
2022-03-04 01:53:33 +0100

    comments

--- displayfunctions.c
+++ displayfunctions.c
@@ -3,20 +3,6 @@
 #include <stdbool.h>
 #include "displayfunctions.h"
 
-// #define DISPLAY_VDD PORTFbits.RF6
-// #define DISPLAY_VBATT PORTFbits.RF5
-// #define DISPLAY_COMMAND_DATA PORTFbits.RF4
-// #define DISPLAY_RESET PORTGbits.RG9
-
-// #define DISPLAY_VDD_PORT PORTF
-// #define DISPLAY_VDD_MASK 0x40
-// #define DISPLAY_VBATT_PORT PORTF
-// #define DISPLAY_VBATT_MASK 0x20
-// #define DISPLAY_COMMAND_DATA_PORT PORTF
-// #define DISPLAY_COMMAND_DATA_MASK 0x10
-// #define DISPLAY_RESET_PORT PORTG
-// #define DISPLAY_RESET_MASK 0x200
-
 #define DISPLAY_CHANGE_TO_COMMAND_MODE (PORTFCLR = 0x10)
 #define DISPLAY_CHANGE_TO_DATA_MODE (PORTFSET = 0x10)
 
@@ -24,9 +10,9 @@
 #define DISPLAY_DO_NOT_RESET (PORTGSET = 0x200)
 
 #define DISPLAY_ACTIVATE_VDD (PORTFCLR = 0x40)
-#define DISPLAY_ACTIVATE_VBAT (PORTFCLR = 0x20)
-
 #define DISPLAY_TURN_OFF_VDD (PORTFSET = 0x40)
+
+#define DISPLAY_ACTIVATE_VBAT (PORTFCLR = 0x20)
 #define DISPLAY_TURN_OFF_VBAT (PORTFSET = 0x20)
 
 const int BALL_REP = 0b00000000000000000000000000000011;
@@ -46,19 +32,15 @@ uint8_t spi_send_recv(uint8_t data) {
 }
 
 void display_init() {
-	//DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
 	DISPLAY_CHANGE_TO_COMMAND_MODE; //clear and set to be able to send command
 	delay(10);
-	//DISPLAY_VDD_PORT &= ~DISPLAY_VDD_MASK;
-	DISPLAY_ACTIVATE_VDD; //turn on power
+	DISPLAY_ACTIVATE_VDD; //turn on power (VDD)
 
 	delay(1000000);
 	
 	spi_send_recv(0xAE);
-	//DISPLAY_RESET_PORT &= ~DISPLAY_RESET_MASK;
 	DISPLAY_ACTIVATE_RESET;
 	delay(10);
-	//DISPLAY_RESET_PORT |= DISPLAY_RESET_MASK;
 	DISPLAY_DO_NOT_RESET;
 	delay(10);
 	
@@ -68,9 +50,8 @@ void display_init() {
 	spi_send_recv(0xD9);
 	spi_send_recv(0xF1);
 	
-	//DISPLAY_VBATT_PORT &= ~DISPLAY_VBATT_MASK;
 	//turn on vcc
-    DISPLAY_ACTIVATE_VBAT;
+    DISPLAY_ACTIVATE_VBAT; //turn on VCC
 	delay(10000000);
 	
 	spi_send_recv(0xA1);
@@ -107,16 +88,15 @@ void display_image(int x, const uint8_t *data) {
 	int i, j;
 	
 	for(i = 0; i < 4; i++) {
-		//DISPLAY_COMMAND_DATA_PORT &= ~DISPLAY_COMMAND_DATA_MASK;
 		DISPLAY_CHANGE_TO_COMMAND_MODE; //set to recieve command
 
-		spi_send_recv(0x22);
-		spi_send_recv(i);
+		spi_send_recv(0x22);	//Set page command
+		spi_send_recv(i);		//page number
 		
-		spi_send_recv(x & 0xF);
-		spi_send_recv(0x10 | ((x >> 4) & 0xF));
+		//start at the left  column
+		spi_send_recv(x & 0xF);						//set low nybble of column
+		spi_send_recv(0x10 | ((x >> 4) & 0xF));		//set high nybble of column
 		
-		//DISPLAY_COMMAND_DATA_PORT |= DISPLAY_COMMAND_DATA_MASK;
 		DISPLAY_CHANGE_TO_DATA_MODE;
 
 		for(j = 0; j < 32*4; j++)
